28193a529605025d80ead6778f2d43c939291dff
Konstantin Mamaev <kostya.plhs@gmail.com>
2017-03-22 22:47:57 +0700

    Add bridging header.

---
+++ FlickrLite/FlickrLite-Bridging-Header.h
@@ -0,0 +1,7 @@
+#ifndef FlickrLite_Bridging_Header_h
+#define FlickrLite_Bridging_Header_h
+
+#import <NYTPhotoViewer/NYTPhoto.h>
+#import <NYTPhotoViewer/NYTPhotosViewController.h>
+
+#endif /* FlickrLite_Bridging_Header_h */
